0aab964234db498ed3be1a3508135d8bb40024aa
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2014-08-05 22:38:34 -0400

    cleaned up code. removed select comments

--- additive_prime.rb
+++ additive_prime.rb
@@ -19,12 +19,12 @@ class FindsIfAdditive
 		Prime.each(n) do |prime|
 			@list_of_primes << prime
 		end
-		print @list_of_primes
+		# print @list_of_primes
 	end
 
 	def find_if_additive(x, y)
 			unless secret(x+y) == secret(x) + secret(y)
-				#break
+				
 				puts "this is NOT additive"
 			end
 	end
@@ -34,20 +34,13 @@ class FindsIfAdditive
 		@list_of_prime_combos = Array.new
 		while a < @list_of_primes.length do
 			b = a + 1
-			while b < @list_of_primes.length do
-				# @iteration = Array.new
+			until b == @list_of_primes.length do
 				find_if_additive(@list_of_primes[a], @list_of_primes[b])
-				# what do i want here?
-				# i want it to break if additive = false
-				# output to the terminal that it broke
-				# @iteration << @list_of_primes[a]
-				# @iteration << @list_of_primes[b]
-				# @list_of_prime_combos << @iteration
 				b += 1
 			end
 			a += 1
 		end
-		puts "Hurray! We made it through the checker"
+		puts "Hurray! We made it through the checker."
 		puts "That means the secret function is Additive!"
 		#print @list_of_prime_combos
 	end
